91a74152599b822ae7bf68845a7896a060989e5c
Mouhine <98873430+Mouhine@users.noreply.github.com>
2023-02-09 14:58:39 +0100

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -4,7 +4,7 @@ import AboutInfo from "./AboutInfo";
 
 const About = () => {
   const SRC =
-    "https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60";
+    "https://cdn.pixabay.com/photo/2017/06/13/22/42/kitchen-2400367_960_720.jpg";
   return (
     <div className=" w-full  lg:w-[74%] mx-auto lg:h-[600px] place-items-center  grid grid-cols-1 gap-8 md:grid-cols-4  relative">
       <HeroImage src={SRC} />
