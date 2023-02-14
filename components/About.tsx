db8f9b8e445abbede1c97872f1da5297b04966b5
Mouhine <98873430+Mouhine@users.noreply.github.com>
2023-02-14 18:24:04 +0000

    modefid img

--- components/About.tsx
+++ components/About.tsx
@@ -3,11 +3,9 @@ import HeroImage from "./HeroImage";
 import AboutInfo from "./AboutInfo";
 
 const About = () => {
-  const SRC =
-    "https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60";
   return (
     <div className=" w-full  lg:w-[74%] mx-auto lg:h-[600px] place-items-center  grid grid-cols-1 gap-8 md:grid-cols-4  relative">
-      <HeroImage src={SRC} />
+      <HeroImage src={"/bg2.jpg"} />
       <AboutInfo />
     </div>
   );
--- components/Intro_Section.tsx
+++ components/Intro_Section.tsx
@@ -4,13 +4,13 @@ import HeroImage from "./HeroImage";
 
 const Intro_Section = () => {
   const SRC =
-    " https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=875&q=80";
+    "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=875&q=80";
   return (
     <div className="w-full bg-[#101010]  lg:min-h-[80vh] pb-16 pt-[70px] ">
       <div className=" w-full lg:w-[75%] mx-auto px-4  grid grid-cols-1 gap-8 md:grid-cols-4  relative">
         <div className="h-[150px] max-w-[150px] hidden md:block rounded-full left-[230px] top-8 bg-[#e46a18] absolute"></div>
         <HeroInfo />
-        <HeroImage src={SRC} />
+        <HeroImage src={"/bg.jpg"} />
       </div>
     </div>
   );
