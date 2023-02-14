7da63cf6abc701cb3647c4bd793ce1639d23ff4e
Mouhine <98873430+Mouhine@users.noreply.github.com>
2023-02-14 19:26:40 +0100

    Update Intro_Section.tsx

--- components/Intro_Section.tsx
+++ components/Intro_Section.tsx
@@ -4,7 +4,7 @@ import HeroImage from "./HeroImage";
 
 const Intro_Section = () => {
   const SRC =
-    " https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=875&q=80";
+    "https://cdn.pixabay.com/photo/2017/06/13/22/42/kitchen-2400367_960_720.jpg";
   return (
     <div className="w-full bg-[#101010]  lg:min-h-[80vh] pb-16 pt-[70px] ">
       <div className=" w-full lg:w-[75%] mx-auto px-4  grid grid-cols-1 gap-8 md:grid-cols-4  relative">
